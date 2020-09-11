f3adb1e51f559de10ace7e3f350c944d3e3889d3
Cameron Moore <cam@cameronlpmoore.com>
2020-09-11 10:31:15 -0700

    update 1_1.js comments

--- ch_1/1_1.js
+++ ch_1/1_1.js
@@ -1,5 +1,9 @@
-// Conditional expression
-// predicate ? consequent expression : alternative expression;
+/* 
+* Conditional expressions
+To evaluate a conditional expression, the interpreter first evaluates the predicate part
+of the expression. If the predicate evaluates to true, the interpreter evaluates consequent-expression. Otherwise it evaluates alternative-expression.
+* predicate ? consequent-expression : alternative expression
+*/
 
 function abs(x) {
   return x >= 0 ? x : -x;
